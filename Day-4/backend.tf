5606041953fd69af463fe9d6299f4dfbbc2f9a4b
shashank753 <shashanksiddappa@gmail.com>
2024-09-27 18:43:26 +0530

    Update backend.tf

--- Day-4/backend.tf
+++ Day-4/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "abhishek-s3-demo-xyz" # change this
-    key            = "/terraform-zero-to-hero/day-4/terraform.tfstate"
+    key            = "terraform-zero-to-hero/day-4/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "terraform-lock"
